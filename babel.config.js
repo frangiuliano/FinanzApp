d15376571ffd803278279fde4f416908c0449da3
frangiuliano <francosebastiangiuliano@gmail.com>
2024-03-16 13:25:51 -0300

    Project startup and NativeWind configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      'babel-plugin-postcss',
+      {
+        plugins: {
+          'postcss-preset-env': {},
+          'postcss-nested': {},
+        },
+      },
+    ],
+  ],
 };
